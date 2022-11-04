36cd2c3e3c8e07f26ccf8273c723d8fcf97aee18
Jannik Braun <jannikb03@gmail.com>
2022-11-04 22:51:23 +0100

    first version

--- inference.py
+++ inference.py
@@ -1,24 +1,33 @@
+import os
 import torch
 from library.models import AE, AutoEncoder, DeepAutoEncoder
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 from torchsummary import summary    
+from datetime import datetime
+# My Modules
+from library.utils import makedir
 
-PATH = './models/model_BCEWithLogitsLoss.pt'
+
+date = datetime.today().strftime('%Y%m%d_%H%M%S') 
+
+PATH = './results/training/01_Examples/100_Epochs_linear_AE/model.pt'
+dir_results = makedir('results')
+dir_inference = makedir('inference', dir_results)
+dir_run = makedir(f'run_{date}',dir_inference)
 
 transform = ToTensor()
-testset = MNIST(root='./', train=False, transform=transform) 
+testset = MNIST(root='datasets/', train=False, transform=transform) 
 
 input_size = testset[0][0].size()
 input_size_flatten = input_size[1]*input_size[2]
-embedding_size = 32
-model = DeepAutoEncoder(input_dim=input_size_flatten, encoding_dim=embedding_size)
-# model = AE(input_size=input_size_flatten, embedding_size=embedding_size)
-model.load_state_dict(torch.load(PATH))
 
+model = torch.jit.load(PATH)
 model.eval()
 print(model)
+
+fig, axs = plt.subplots(2, 10)
 
 for i in range(10):
     data, _ = testset[i]
@@ -26,7 +35,8 @@ for i in range(10):
     recon = model(data_flatten)
     recon = recon.view(input_size[1], input_size[2])
     data = torch.squeeze(data)
-    fig, axs = plt.subplots(2,1)
-    axs[0].imshow(data.detach().numpy())
-    axs[1].imshow(recon.detach().numpy())
-    plt.show()
+    axs[0][i].imshow(data.detach().numpy())
+    axs[0][i].axis('off')
+    axs[1][i].imshow(recon.detach().numpy())
+    axs[1][i].axis('off')
+    plt.savefig(os.path.join(dir_run, 'results.svg'))
