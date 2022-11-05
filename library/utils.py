a54fee60355d79319aedef83f4764d0590bd5001
Jannik Braun <jannikb03@gmail.com>
2022-11-05 12:47:15 +0100

    created class for progress image generation

--- library/utils.py
+++ library/utils.py
@@ -90,3 +90,43 @@ class SaveBestModel:
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': criterion,
                 }, model_path)
+
+
+class Progress_images:
+    def __init__(self, num_epochs):
+        self.num_epochs = num_epochs
+        num_progress_steps = 5
+        self.step_size = int(num_epochs/num_progress_steps)
+        fig = plt.figure()
+        subfigs = fig.subfigures(2,1)
+        subfigs[0].suptitle('Ground Truth')
+        subfigs[1].suptitle('Reconstruction')
+        self.axs_top = subfigs[0].subplots(1)
+        if num_epochs%self.step_size == 0:
+            self.axs_bottom = subfigs[1].subplots(1, num_progress_steps)
+        else:
+            self.axs_bottom = subfigs[1].subplots(1, num_progress_steps+1)
+        self.cnt = 0
+
+    def __call__(self, model, testset, epoch):
+        if epoch%self.step_size == 0 or epoch == self.num_epochs:
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            model.to(device)
+            img = testset[0][0].to(device)
+            img = torch.flatten(img, start_dim=1)
+            recon = model(img)
+            recon = torch.reshape(recon,(28, 28))
+            recon = recon.cpu().detach().numpy()
+            self.axs_bottom[self.cnt].imshow(recon)
+            self.axs_bottom[self.cnt].set_title(f'Epoch {epoch+1}')
+            self.axs_bottom[self.cnt].axis('off')
+            self.cnt += 1
+        if epoch == 0:
+            # finalize progress results and save it
+            img = torch.reshape(img,(28, 28))
+            img = img.cpu().detach().numpy()
+            self.axs_top.imshow(img)
+            self.axs_top.axis('off')
+
+    def save(self, path):
+        plt.savefig(os.path.join(path, 'Reconstruction progress.svg'))
