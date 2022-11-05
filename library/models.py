85f996748f86577bc56acdfe21f7f8e78a54aa9b
Jannik Braun <jannikb03@gmail.com>
2022-11-05 20:10:32 +0100

    splitted AE Model class in Encoder and Decoder

--- library/models.py
+++ library/models.py
@@ -2,9 +2,36 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
-class DeepAutoEncoder(nn.Module):
+class Linear_AE(nn.Module):
     def __init__(self, input_dim, encoding_dim):
-        super(DeepAutoEncoder, self).__init__()
+        super().__init__()
+        self.encoder = Linear_Encoder(input_dim=input_dim, encoding_dim=encoding_dim)
+        self.decoder = Linear_Decoder(output_dim=input_dim, encoding_dim=encoding_dim)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class Linear_Decoder(nn.Module):
+    def __init__(self, encoding_dim, output_dim) -> None:
+        super().__init__()
+        self.decoder = nn.Sequential(
+            nn.Linear(encoding_dim, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True), 
+            nn.Linear(128, output_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, encoded):
+        decoded = self.decoder(encoded)
+        return decoded
+        
+class Linear_Encoder(nn.Module):
+    def __init__(self, input_dim, encoding_dim) -> None:
+        super().__init__()
         self.encoder = nn.Sequential(
             nn.Linear(input_dim, 128),
             nn.ReLU(True),
@@ -13,94 +40,53 @@ class DeepAutoEncoder(nn.Module):
             nn.Linear(64, encoding_dim), 
             nn.ReLU(True),
         )
-        self.decoder = nn.Sequential(
-            nn.Linear(encoding_dim, 64),
-            nn.ReLU(True),
-            nn.Linear(64, 128),
-            nn.ReLU(True), 
-            nn.Linear(128, input_dim),
-            # nn.Tanh()
-        )
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        encoded = self.encoder(x)
+        return encoded
 
-class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, encoding_dim):
-        super(AutoEncoder, self).__init__()
-        self.encoder = nn.Linear(input_dim, encoding_dim)
-        self.decoder = nn.Linear(encoding_dim, input_dim)
-
-    def forward(self, x):
-        encoded = F.relu(self.encoder(x))
-        decoded = self.decoder(encoded)
-        return decoded, encoded
-
-
-class AE(nn.Module):
-    def __init__(self, input_size, embedding_size) -> None:
+class CNN_AE(nn.Module):
+    def __init__(self) -> None:
         super().__init__()
-        num_layers = 2
-        second_layer = int((input_size-embedding_size)/num_layers)
-
-        self.encoder = nn.Sequential(
-            nn.Linear(input_size, second_layer),
-            nn.ReLU(),
-            nn.Linear(second_layer, embedding_size),
-            nn.Sigmoid(),
-        )
-
-        self.decoder = nn.Sequential(
-            nn.Linear(embedding_size, second_layer),
-            nn.ReLU(),
-            nn.Linear(second_layer, input_size),
-            #nn.Sigmoid(),
-        )
-
+        self.encoder = CNN_Encoder()
+        self.decoder = CNN_Decoder()
+    
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
 
-class VariationalEncoder(nn.Module):
-    def __init__(self, latent_dims):
-        super(VariationalEncoder, self).__init__()
-        self.linear1 = nn.Linear(784, 512)
-        self.linear2 = nn.Linear(512, latent_dims)
-        self.linear3 = nn.Linear(512, latent_dims)
-
-        self.N = torch.distributions.Normal(0, 1)
-        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
-        self.N.scale = self.N.scale.cuda()
-        self.kl = 0
-
+class CNN_Encoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, 2),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, 2),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(6*6*32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 4),
+        )
+    
     def forward(self, x):
-        x = torch.flatten(x, start_dim=1)
-        x = F.relu(self.linear1(x))
-        mu =  self.linear2(x)
-        sigma = torch.exp(self.linear3(x))
-        z = mu + sigma*self.N.sample(mu.shape)
-        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
-        return z
-
-class Decoder(nn.Module):
-    def __init__(self, latent_dims):
-        super(Decoder, self).__init__()
-        self.linear1 = nn.Linear(latent_dims, 512)
-        self.linear2 = nn.Linear(512, 784)
-
-    def forward(self, z):
-        z = F.relu(self.linear1(z))
-        z = torch.sigmoid(self.linear2(z))
-        return z.reshape((-1, 1, 28, 28))
-
-class VariationalAutoencoder(nn.Module):
-    def __init__(self, latent_dims):
-        super(VariationalAutoencoder, self).__init__()
-        self.encoder = VariationalEncoder(latent_dims)
-        self.decoder = Decoder(latent_dims)
-
+        encoded = self.encoder(x)
+        return encoded
+    
+class CNN_Decoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 128),
+            nn.ReLU(),
+            nn.Linear(128, 6*6*32),
+            nn.ReLU(),
+            nn.Unflatten(1, (32, 6, 6)),
+            nn.ConvTranspose2d(32, 16, 3, 2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 4, 2),
+            nn.Sigmoid()
+        )    
     def forward(self, x):
-        z = self.encoder(x)
-        return self.decoder(z)
+        decoded = self.decoder(x)
+        return decoded
